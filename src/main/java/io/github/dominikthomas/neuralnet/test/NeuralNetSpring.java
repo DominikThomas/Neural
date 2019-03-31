6ad268044d8d413c02843f5024a79e37582676b1
DominikThomas <dominik.v.thomas@gmail.com>
2019-03-31 16:43:15 +0200

    Change package name.

---
+++ src/main/java/io/github/dominikthomas/neuralnet/test/NeuralNetSpring.java
@@ -0,0 +1,49 @@
+package io.github.dominikthomas.neuralnet.test;
+
+import java.util.Arrays;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import io.github.dominikthomas.neuralnet.beans.NeuralNet;
+import io.github.dominikthomas.neuralnet.math.RandomNumberGenerator;
+
+/**
+ *
+ * NeuralNetConsoleTest
+ * This class is solely used for creating and testing your very first Neural 
+ * Network in Java 
+ *
+ * @author Alan de Souza, Fábio Soares, Dominik Thomas
+ */
+public class NeuralNetSpring {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+ 
+		RandomNumberGenerator.setSeed(2);
+		
+		// loading the definitions from the given XML file
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"neuralnetspring/applicationContext.xml");
+ 
+		NeuralNet nn = (NeuralNet) context
+				.getBean("neuralNet");
+		
+        double [] neuralInput = { 1.5 , 0.5 };
+        
+        double [] neuralOutput;
+        nn.setInputs(neuralInput);
+        nn.calc();
+        neuralOutput=nn.getOutputs();
+        System.out.println(Arrays.toString(neuralOutput));
+
+        neuralInput[0] = 1.0;
+        neuralInput[1] = 2.1;
+        
+        nn.setInputs(neuralInput);
+        nn.calc();
+        neuralOutput=nn.getOutputs();
+        System.out.println(Arrays.toString(neuralOutput));
+        
+    }
+}
